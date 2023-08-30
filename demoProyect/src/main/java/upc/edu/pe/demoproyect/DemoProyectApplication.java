fbf10858d3f5e85205588c574b36324df3714040
Pana Leonardo <leonardo474lm@gmail.com>
2023-08-30 14:54:25 -0500

    agregadno

---
+++ demoProyect/src/main/java/upc/edu/pe/demoproyect/DemoProyectApplication.java
@@ -0,0 +1,13 @@
+package upc.edu.pe.demoproyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProyectApplication.class, args);
+	}
+
+}
