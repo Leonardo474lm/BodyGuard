63262f014510efa6749dad1bc2c6b3a21319b116
Cesar7321 <choccataolivera05@gmail.com>
2023-11-10 20:35:49 -0500

    Fix getRoleByUser

--- demoProyect/src/main/java/upc/edu/pe/demoproyect/repository/RoleRepository.java
+++ demoProyect/src/main/java/upc/edu/pe/demoproyect/repository/RoleRepository.java
@@ -1,9 +1,11 @@
 package upc.edu.pe.demoproyect.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import upc.edu.pe.demoproyect.entities.Role;
 @Repository
 public interface RoleRepository extends JpaRepository<Role,Long> {
+    @Query("SELECT r FROM Role r WHERE r.user.id = :id")
     public Role findByUser_Id(int id);
 }
